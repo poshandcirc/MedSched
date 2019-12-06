014f31470cba26f86cc3ec8c95f8bfd1874f4219
Cassandra Stone <cstone2@swarthmore.edu>
2019-12-06 18:18:03 -0500

    first commit with template files

---
+++ MedSched/MedSchedTests/MedSchedTests.swift
@@ -0,0 +1,34 @@
+//
+//  MedSchedTests.swift
+//  MedSchedTests
+//
+//  Created by Cassandra Stone on 12/6/19.
+//  Copyright © 2019 Cassandra Stone. All rights reserved.
+//
+
+import XCTest
+@testable import MedSched
+
+class MedSchedTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
